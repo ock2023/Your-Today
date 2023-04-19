a0c9dc09cf5bac720bfd7738e69b8b76d02d2da3
ock2023 <dmsdhr2605@gmail.com>
2023-04-19 23:09:24 +0900

    test

---
+++ Downloads/seb43_pre_026-dev-front/seb43_pre_026-dev-front/client/src/components/HomeList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import HomeItem from './HomoItem';
+
+const HomeList = () => {
+  return (
+    <div>
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+      <HomeItem />
+    </div>
+  );
+};
+
+export default HomeList;
---
+++ Downloads/seb43_pre_026-dev-front/seb43_pre_026-dev-front/client/src/pages/Questions.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Question from '../components/Question';
+
+const Questions = () => {
+  return (
+    <div>
+      <Question />
+    </div>
+  );
+};
+
+export default Questions;
